1f58ac06dcd0b12bc5ee4b626fd91c14a6cf85ea
Jeong jinyoung <jjyoung2419@gmail.com>
2023-10-29 14:13:34 +0900

    step 4, print no, out of loop block

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -4,25 +4,29 @@ import java.util.Scanner;
 
 class App {
     void run() {
+        System.out.println("== 명언 앱 ==");
+
+        int lastQuotationId = 0;
+
         while (true) {
-            System.out.println("== 명언 앱 ==");
             System.out.print("명령) ");
 
             Scanner scanner = new Scanner(System.in);
             String cmd = scanner.nextLine();
 
-            if (cmd.equals("종료")){
+            if (cmd.equals("종료")) {
                 break;
-            }
-            else if (cmd.equals("등록")){
+            } else if (cmd.equals("등록")) {
                 System.out.print("명언 : ");
                 String content = scanner.nextLine();
-
                 System.out.print("작가 : ");
                 String authorName = scanner.nextLine();
 
-                System.out.printf("명언 : %s, 작가 : %s\n", content, authorName);
+                lastQuotationId++;
+
+                System.out.printf("%d번 명언이 등록되었습니다.\n", lastQuotationId);
             }
         }
+
     }
 }
